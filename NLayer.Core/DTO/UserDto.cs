303f08cfc1efe4dd1f4a4ef648812c80d2f8e520
Ahmet Emre Karaca <ahmetemre_k@hotmail.com>
2022-12-30 13:10:43 +0300

    Proje dosyası ekle.

---
+++ NLayer.Core/DTO/UserDto.cs
@@ -0,0 +1,15 @@
+﻿using NLayer.Core.Models;
+
+
+namespace NLayer.Core.DTO
+{
+    public class UserDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public ICollection<Todo>? Todos { get; set; } 
+    }
+}
---
+++ WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTO;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<User> _service;
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(UserDto request)
+        {
+            var userSave = await _service.AddAsync(_mapper.Map<User>(request));
+            var usersDto = _mapper.Map<UserDto>(userSave);
+            return Ok(usersDto);
+            
+        }
+    }
+}
